7a01c0f66b598d5a9882cac7de90ef5571c37b29
Mohammed <Mhds.alshammeri@gmail.com>
2026-09-02 21:33:45 +0300

    Project init

---
+++ Sharek/Sharek/SharekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharekApp.swift
+//  Sharek
+//
+//  Created by Mohammed on 02/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SharekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
